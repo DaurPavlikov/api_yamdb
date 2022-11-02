d033ba2c80005057f3339f49d7b9165034bcf8a5
Kot9lpa99 <kot9lpa99@yandex.ru>
2022-11-02 23:21:15 +0300

    small fix, imports

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,60 +5,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from users.models import User
 from reviews.validators import validate_year
 
+TEN = 10
+ONE = 1
+TWENTY = 20
 
-class Review(models.Model):
-    title = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-        verbose_name='Название произведения')
-    text = models.TextField(verbose_name='Текст обзора')
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор')
-    score = models.PositiveSmallIntegerField(
-        default=0,
-        validators=[MinValueValidator(1), MaxValueValidator(10)],
-        verbose_name='Оценка')
-    pub_date = models.DateTimeField(auto_now_add=True,
-                                    verbose_name='Дата обзора')
-
-    class Meta():
-        verbose_name = 'Обзор'
-        verbose_name_plural = 'Обзоры'
-        constraints = [
-            models.UniqueConstraint(
-                fields=['title', 'author'], name='unique')]
-
-    def __str__(self):
-        self.text[:20]
-
-
-class Comment(models.Model):
-    review = models.ForeignKey(
-        Review,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Обзор'
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор'
-    )
-    text = models.TextField(
-        verbose_name='Текст комментария',)
-    pub_date = models.DateTimeField(auto_now_add=True,
-                                    verbose_name='Дата обзора')
-
-    class Meta():
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
-
-    def __str__(self):
-        return self.text[:15]
-        
 
 class Genre(models.Model):
     """Модель жанры, многие ко многим"""
@@ -115,3 +65,57 @@ class GenreTitle(models.Model):
 
     def __str__(self) -> str:
         return f'{self.genre} {self.title}'
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Название произведения')
+    text = models.TextField(verbose_name='Текст обзора')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор')
+    score = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[MinValueValidator(ONE), MaxValueValidator(TEN)],
+        verbose_name='Оценка')
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name='Дата обзора')
+
+    class Meta():
+        verbose_name = 'Обзор'
+        verbose_name_plural = 'Обзоры'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'], name='unique')]
+
+    def __str__(self):
+        self.text[:TWENTY]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Обзор'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    text = models.TextField(
+        verbose_name='Текст комментария',)
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name='Дата обзора')
+
+    class Meta():
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text[:TWENTY]
+        
